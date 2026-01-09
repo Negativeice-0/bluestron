c9d702dd5387183c1ac0490978be74a7e719cbcd
Billy Gates Osodo Ochieng <your@email.com>
2026-01-09 12:34:50 +0300

    backend extracted

---
+++ bluestron-backend/src/main/java/com/bluestron/ecosystem/BluestronBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bluestron.ecosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluestronBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluestronBackendApplication.class, args);
+	}
+
+}
---
+++ bluestron-backend/src/test/java/com/bluestron/ecosystem/BluestronBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluestron.ecosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluestronBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
