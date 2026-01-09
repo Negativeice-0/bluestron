3f20c1ab79b08f18767e0f0e862039201027fc18
Billy Gates Osodo Ochieng <your@email.com>
2026-01-09 15:43:38 +0300

    backend minimal setup complete

---
+++ bsapi/src/main/java/co/ke/bluestron/bsapi/BsapiApplication.java
@@ -0,0 +1,13 @@
+package co.ke.bluestron.bsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsapiApplication.class, args);
+	}
+
+}
---
+++ bsapi/src/test/java/co/ke/bluestron/bsapi/BsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ke.bluestron.bsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
