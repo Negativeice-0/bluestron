3286e198c9c8d1c66519c1e497245c318da2c77e
Billy Gates Osodo Ochieng <your@email.com>
2026-01-10 08:14:07 +0300

    massive overhaul -- 59 files

--- bsapi/src/main/java/co/ke/bluestron/bsapi/repository/ServiceEnquiryRepository.java
+++
@@ -1,9 +0,0 @@
-package co.ke.bluestron.bsapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.ke.bluestron.bsapi.entity.ServiceEnquiry;
-
-public interface ServiceEnquiryRepository extends JpaRepository<ServiceEnquiry, Long> {
-    // Basic CRUD operations provided by JpaRepository
-}
--- bsapi/src/main/java/co/ke/bluestron/bsapi/repository/VenueRepository.java
+++
@@ -1,9 +0,0 @@
-package co.ke.bluestron.bsapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.ke.bluestron.bsapi.entity.Venue;
-
-public interface VenueRepository extends JpaRepository<Venue, Long> {
-    // Basic CRUD operations provided by JpaRepository
-}
